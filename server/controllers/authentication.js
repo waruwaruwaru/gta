e988569420cb9722a857f5c1881a0b3dbbec51a2
waru <wtran@ucdavis.edu>
2017-11-18 14:40:22 -0800

    hashed password

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,13 +1,16 @@
 const { Client } = require('pg');
 const connectionString = 'jdbc:postgresql://gtapocdbb.c7o752b846hb.us-east-1.rds.amazonaws.com:5432/gtapocdbb'
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 
 exports.signup = function(req, res) {
-  const firstname = req.body.firstname;
-  const lastname = req.body.lastname;
-  const dob = req.body.dob;
-  const university_id = req.body.university_id;
-  const email = req.body.email; //university email
-  const password = req.body.password;
+  var firstname = req.body.firstname;
+  var lastname = req.body.lastname;
+  var dob = req.body.dob;
+  var university_id = req.body.university_id;
+  var email = req.body.email; //university email
+  var password = req.body.password;
 
   var client = new Client({
     user: 'six',
@@ -17,22 +20,22 @@ exports.signup = function(req, res) {
     port: 5432,
   })
   client.connect();
-  client.query("SELECT FROM users WHERE email = $1", [email])
-    .then(function(data) {
-      console.log('data: ', data);
-      if (data.rowCount == 0) {
-        client.query("INSERT INTO users(firstname, lastname, dob, universityid, email, password) VALUES($1, $2, $3, $4, $5, $6)",
-        [firstname, lastname, dob, university_id, email, password])
-        .then(() => { res.send("DONE")});
-      } else {
-        res.send("EMAIL ALREADY EXISTS");
-      }
-    });
-
-
-
+  bcrypt.hash(password, saltRounds, function(err, hash) {
+    // Store hash in your password DB.
+    client.query("SELECT FROM users WHERE email = $1", [email])
+      .then(function(data) {
+        if (data.rowCount == 0) {
+          client.query("INSERT INTO users(firstname, lastname, dob, universityid, email, password) VALUES($1, $2, $3, $4, $5, $6)",
+          [firstname, lastname, dob, university_id, email, hash])
+          .then(() => { res.send("DONE")});
+        } else {
+          res.send("EMAIL ALREADY EXISTS");
+        }
+      });
+  });
 
 };
+
 
 exports.signin = function(req, res) {
 
